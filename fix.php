b68b891aed9bacf69c2bc949f37cccc43bc1f901
ssds80 <123647844+ssds80@users.noreply.github.com>
2025-05-23 19:55:27 +0800

    Update fix.php

--- fix.php
+++ fix.php
@@ -462,7 +462,7 @@ input.btn{font-weight: bold; height: 20px; line-height: 20px; padding: 0 6px; co
 .word{word-break:break-all;}
 -->
 </style>
-<script language="JavaScript" type="text/javascript" src="http://lib.sinaapp.com/js/jquery/1.7/jquery.min.js"></script>
+<script language="JavaScript" type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script type="text/javascript">
 <!--
 $(document).ready(function(){getJSONData();});
